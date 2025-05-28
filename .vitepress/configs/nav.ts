f0c773463a5c8e01bcd64b8262a7cda6684c2170
eason2young <752878898@qq.com>
2025-05-28 12:50:18 +0800

    增加育儿书籍

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -4,6 +4,6 @@ import type { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.Config['nav'] = [
     { text: '首页', link: '/' },
     { text: '知识库', link: '/01 知识库' },
-    { text: '播客', link: '/03 新手爸妈播客' },
     { text: '资料库', link: '/02 资料库' },
+    { text: '播客', link: '/03 新手爸妈播客' },
 ]
